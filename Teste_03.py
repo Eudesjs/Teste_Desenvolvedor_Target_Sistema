b8b6024ec6e2d07805708182ec5fb6b9182802bd
eudes <eudsjs@gmail.com>
2024-09-30 00:24:08 -0300

    Conclusão do Teste 03

--- Teste_03.py
+++ Teste_03.py
@@ -9,9 +9,27 @@ IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 '''
+import json
 
-import pandas as pd 
+def calcular_faturamento(arquivo):
+    with open(arquivo, "r") as f:
+       dados = json.load(f)
+    
+    faturamentos = [item["valor"] for item in dados if item["valor"] > 0]
 
-df = pd.read_json("https://drive.google.com/file/d/1qXvCPjEL4jerElN-hnScoKkEVmSQ8A2L/view?usp=sharing")
+    menor_faturamento = min(faturamentos)
+    maior_faturamento = max(faturamentos)
+    media_faturamento = sum(faturamentos) / len(faturamentos)
 
-print(df)
+    dias_acima_media = sum(1 for valor in faturamentos if valor > media_faturamento)
+
+    return menor_faturamento, maior_faturamento, dias_acima_media
+
+arquivo = "Teste_Desenvolvedor_Target_Sistemas/dados.json"
+menor_faturamento, maior_faturamento, dias_acima_media = calcular_faturamento(arquivo)
+
+print(f"Maior Valor de Faturamento: R$ {maior_faturamento:.2f}")
+print(f"Menor Valor de Faturmanento: R$ {menor_faturamento:.2f}")
+print(f"Número de Dias com Faturamento acima de Média: R$ {dias_acima_media:.2f}")
+
+
--- import_Json.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_json("https://drive.google.com/file/d/1qXvCPjEL4jerElN-hnScoKkEVmSQ8A2L/view?usp=sharing") 
-
-print(df)
